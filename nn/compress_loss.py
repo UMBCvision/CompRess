b9ec6ea7a303a46d5d179e3652e92c760742d033
Soroush Abbasi Koohpayegani <62820830+soroush-abbasi@users.noreply.github.com>
2020-06-29 16:10:57 -0400

    Add files via upload

--- nn/compress_loss.py
+++ nn/compress_loss.py
@@ -2,17 +2,13 @@ import torch
 from torch import nn
 import math
 import torch.nn.functional as F
-import pdb
-
-def normalize(x):
-    return x / x.norm(2, dim=1, keepdim=True)
 
 
+class Teacher:
 
-class Teacher():
-    def __init__(self , cached=True , cached_feats=None , model=None):
+    def __init__(self, cached=True, cached_feats=None, model=None):
         self.model = model
-        self.cached_feats=cached_feats
+        self.cached_feats = cached_feats
         self.cached = cached
 
     def eval(self):
@@ -23,11 +19,11 @@ class Teacher():
         if not self.cached:
             self.model = torch.nn.DataParallel(self.model).cuda()
 
-    def forward(self , x, indices):
-        if self.cached :
+    def forward(self, x, indices):
+        if self.cached:
             feats = self.cached_feats[indices]
             feats = feats.cuda()
-        else :
+        else:
             with torch.no_grad():
                 feats = self.model(x)
 
@@ -36,6 +32,7 @@ class Teacher():
 
 
 class SampleSimilarities(nn.Module):
+
     def __init__(self, feats_dim, queueSize, T):
         super(SampleSimilarities, self).__init__()
         self.inputSize = feats_dim
@@ -46,20 +43,15 @@ class SampleSimilarities(nn.Module):
         self.register_buffer('memory', torch.rand(self.queueSize, feats_dim).mul_(2 * stdv).add_(-stdv))
         print('using queue shape: ({},{})'.format(self.queueSize, feats_dim))
 
-
     def forward(self, q):
         batchSize = q.shape[0]
-
-
         queue = self.memory.clone()
         out = torch.mm(queue.detach(), q.transpose(1, 0))
         out = out.transpose(0, 1)
         out = torch.div(out, self.T)
         out = out.squeeze().contiguous()
 
-
-
-        # update memory
+        # update memory bank
         with torch.no_grad():
             out_ids = torch.arange(batchSize).cuda()
             out_ids += self.index
@@ -71,18 +63,15 @@ class SampleSimilarities(nn.Module):
         return out
 
 
-
-
 class CompReSS(nn.Module):
+
     def __init__(self , teacher_feats_dim, student_feats_dim, queue_size=128000, T=0.04):
         super(CompReSS, self).__init__()
-
 
         self.l2norm = Normalize(2).cuda()
         self.criterion = KLD().cuda()
         self.student_sample_similarities = SampleSimilarities(student_feats_dim , queue_size , T).cuda()
         self.teacher_sample_similarities = SampleSimilarities(teacher_feats_dim , queue_size , T).cuda()
-
 
     def forward(self, teacher_feats, student_feats):
 
@@ -93,22 +82,13 @@ class CompReSS(nn.Module):
         similarities_teacher = self.teacher_sample_similarities(teacher_feats)
 
         loss = self.criterion(similarities_teacher , similarities_student)
-
         return loss
 
 
+class SampleSimilaritiesMomentum(nn.Module):
 
-
-
-
-
-
-
-
-
-class SampleSimilarities_moco(nn.Module):
     def __init__(self, feats_dim, queueSize, T):
-        super(SampleSimilarities_moco, self).__init__()
+        super(SampleSimilaritiesMomentum, self).__init__()
         self.inputSize = feats_dim
         self.queueSize = queueSize
         self.T = T
@@ -117,20 +97,15 @@ class SampleSimilarities_moco(nn.Module):
         self.register_buffer('memory', torch.rand(self.queueSize, feats_dim).mul_(2 * stdv).add_(-stdv))
         print('using queue shape: ({},{})'.format(self.queueSize, feats_dim))
 
-
     def forward(self, q , q_key):
         batchSize = q.shape[0]
-
-
         queue = self.memory.clone()
         out = torch.mm(queue.detach(), q.transpose(1, 0))
         out = out.transpose(0, 1)
         out = torch.div(out, self.T)
         out = out.squeeze().contiguous()
 
-
-
-        # update memory
+        # update memory bank
         with torch.no_grad():
             out_ids = torch.arange(batchSize).cuda()
             out_ids += self.index
@@ -142,29 +117,26 @@ class SampleSimilarities_moco(nn.Module):
         return out
 
 
+class CompReSSMomentum(nn.Module):
 
-class CompReSS_moco(nn.Module):
     def __init__(self , teacher_feats_dim, student_feats_dim, queue_size=128000, T=0.04):
-        super(CompReSS_moco, self).__init__()
-
+        super(CompReSSMomentum, self).__init__()
 
         self.l2norm = Normalize(2).cuda()
         self.criterion = KLD().cuda()
-        self.student_sample_similarities = SampleSimilarities_moco(student_feats_dim , queue_size , T).cuda()
-        self.teacher_sample_similarities = SampleSimilarities(teacher_feats_dim , queue_size , T).cuda()
+        self.student_sample_similarities = SampleSimilaritiesMomentum(student_feats_dim, queue_size, T).cuda()
+        self.teacher_sample_similarities = SampleSimilarities(teacher_feats_dim, queue_size, T).cuda()
 
-
-    def forward(self, teacher_feats, student_feats , student_feats_key):
+    def forward(self, teacher_feats, student_feats, student_feats_key):
 
         teacher_feats = self.l2norm(teacher_feats)
         student_feats = self.l2norm(student_feats)
         student_feats_key = self.l2norm(student_feats_key)
 
-        similarities_student = self.student_sample_similarities(student_feats , student_feats_key)
+        similarities_student = self.student_sample_similarities(student_feats, student_feats_key)
         similarities_teacher = self.teacher_sample_similarities(teacher_feats)
 
-        loss = self.criterion(similarities_teacher , similarities_student)
-
+        loss = self.criterion(similarities_teacher, similarities_student)
         return loss
 
 
@@ -179,7 +151,9 @@ class Normalize(nn.Module):
         out = x.div(norm)
         return out
 
+
 class KLD(nn.Module):
+
     def forward(self, targets, inputs):
         targets = F.softmax(targets, dim=1)
         inputs = F.log_softmax(inputs, dim=1)
